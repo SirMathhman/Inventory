1ba784095802af53a2f798921e1709abbe0a3a9a
SirMathhman <sirmathhman@gmail.com>
2021-03-13 23:24:49 -0500

    Add RegisterInput.vue
    Holds the text fields

---
+++ front-end/src/components/RegisterInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-text-field :label="label"
+                :type="password ? 'password' : 'text'"
+                clearable
+                dense
+                outlined>
+  </v-text-field>
+</template>
+<script lang="ts">
+import {Component, Prop, Vue} from "vue-property-decorator";
+
+@Component({})
+export default class RegisterInput extends Vue {
+  @Prop({required: true})
+  private readonly label: string;
+
+  @Prop({default: false})
+  private readonly password: boolean;
+}
+</script>
--- front-end/src/components/RegisterView.vue
+++ front-end/src/components/RegisterView.vue
@@ -1,18 +1,30 @@
 <template>
   <v-card>
-
+    <v-card-title>
+      Register
+    </v-card-title>
+    <v-card-text>
+      <RegisterInput label="Username"/>
+      <RegisterInput :password="true" label="Password"/>
+      <RegisterInput :password="true" label="Confirm Password"/>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn>
+        Submit
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from "vue-property-decorator";
+import RegisterInput from "@/components/RegisterInput.vue";
 
-@Component({})
+@Component({
+  components: {RegisterInput}
+})
 export default class RegisterView extends Vue {
 
 }
 </script>
 
-<style scoped>
-
-</style>
