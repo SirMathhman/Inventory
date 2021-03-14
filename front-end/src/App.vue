a08a16a51dcdf71cd5ad6c713c9429a9b51a0557
SirMathhman <sirmathhman@gmail.com>
2021-03-13 23:15:58 -0500

    Add RegisterView.vue
    Holds the main GUI

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <v-app>
+    <v-main>
+      <RegisterView></RegisterView>
+    </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
+import Vue from "vue";
+import RegisterView from "@/components/RegisterView.vue";
+import {Component} from "vue-property-decorator";
+
+@Component({
+  components: {RegisterView}
+})
+export default class App extends Vue {
+}
 </script>
---
+++ front-end/src/components/RegisterView.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card>
+
+  </v-card>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+
+@Component({})
+export default class RegisterView extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
